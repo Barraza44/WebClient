647cd05ab12376f317f6815e94d1d44858039ade
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-10-31 01:02:05 -0600

    injected web service

--- WebClient/Commands/WebCommand.cs
+++ WebClient/Commands/WebCommand.cs
@@ -4,18 +4,24 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using WebClient.Services;
 
 namespace WebClient.Commands
 {
     public class WebCommand : AsyncCommand<WebCommandSettings>
     {
-        private readonly HttpClient _client = new();
+        private readonly IWebService _webService;
+
+        public WebCommand(IWebService webService)
+        {
+            _webService = webService;
+        }
 
         public override async Task<int> ExecuteAsync(CommandContext context, WebCommandSettings settings)
         {
             try
             {
-                var response = await _client.GetStringAsync(settings.Url);
+                var response = await _webService.GetAsync(settings.Url);
                 AnsiConsole.WriteLine($"{response}");
             }
             catch (Exception e)
