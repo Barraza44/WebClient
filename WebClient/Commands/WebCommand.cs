b258e0a17285e67a9627ec29b02990083b9023cb
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-10-31 01:14:22 -0500

    add error handling

--- WebClient/Commands/WebCommand.cs
+++ WebClient/Commands/WebCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Spectre.Console;
@@ -9,10 +10,20 @@ namespace WebClient.Commands
     public class WebCommand : AsyncCommand<WebCommandSettings>
     {
         private readonly HttpClient _client = new();
+
         public override async Task<int> ExecuteAsync(CommandContext context, WebCommandSettings settings)
         {
-            var response = await _client.GetStringAsync(settings.Url);
-            AnsiConsole.WriteLine($"{response}");
+            try
+            {
+                var response = await _client.GetStringAsync(settings.Url);
+                AnsiConsole.WriteLine($"{response}");
+            }
+            catch (Exception e)
+            {
+                AnsiConsole.MarkupLine($"[red]{e.Message}[/]");
+                return -99;
+            }
+
             return 0;
         }
     }
