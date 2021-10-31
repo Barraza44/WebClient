12c0480af56303a04bc7dc53d6b2dc16afdd181f
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-10-31 01:59:59 -0500

    Add web service for handling requests

---
+++ WebClient/Services/IWebService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace WebClient.Services
+{
+    public interface IWebService
+    {
+        public Task<string> GetAsync(string url);
+
+        public void PostAsync(string url, string body);
+    }
+}
---
+++ WebClient/Services/WebService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebClient.Services
+{
+    public class WebService : IWebService
+    {
+        private readonly HttpClient _client = new();
+        
+        public async Task<string> GetAsync(string url)
+        {
+            return await _client.GetStringAsync(url);
+        }
+
+        public void PostAsync(string url, string body)
+        {
+            
+        }
+    }
+}
