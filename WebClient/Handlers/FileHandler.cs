b09296645aaa20b870328af46636ee12991783e3
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-02 22:29:02 -0600

    add file service to handle IO.

---
+++ WebClient/Handlers/FileHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using WebClient.Services;
+
+namespace WebClient.Handlers
+{
+    public class FileHandler : IFileService 
+    {
+        public async Task SaveAsync(string path)
+        {
+            
+            
+        }
+
+        public Task LoadAsync(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WebClient/Services/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace WebClient.Services
+{
+    public interface IFileService
+    {
+        public Task SaveAsync(string path);
+
+        public Task LoadAsync(string path);
+    }
+}
