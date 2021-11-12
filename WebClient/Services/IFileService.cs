37723b2b824e8665cbb40d53033c3d159ca9a409
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-11 23:30:54 -0600

    Add comments to interfaces and version number to project

--- WebClient/Services/IFileService.cs
+++ WebClient/Services/IFileService.cs
@@ -1,13 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 
 namespace WebClient.Services
 {
     public interface IFileService
     {
+        /// <summary>
+        /// Opens a stream to save text-based data to a file asynchronously. If the file does not exist, it is created
+        /// </summary>
+        /// <param name="path">File on which the data will be saved</param>
+        /// <param name="data">The data to save as text</param>
+        /// <returns>A task representing an asynchronous operation</returns>
         public Task SaveAsync(string path, string data);
 
+        /// <summary>
+        /// Opens a stream to save data, as bytes, to a file asynchronously. If the file does not exist, it is created
+        /// </summary>
+        /// <param name="path">File on which the data will be saved</param>
+        /// <param name="data">The data to save as bytes</param>
+        /// <returns>A task representing an asynchronous operation</returns>
         public Task SaveAsync(string path, byte[] data);
 
+        /// <summary>
+        /// Opens a stream to load data, as text, from a file asynchronously. If the file does not exist, an exception is thrown
+        /// </summary>
+        /// <param name="path">File from which the data will be loaded</param>
+        /// <returns>A tuple containing the file contents as a string, and its extension</returns>
+        /// <exception cref="FileNotFoundException">No such file or directory exists</exception>
         public Task<(string, string)> LoadAsync(string path);
     }
 }
--- WebClient/Services/IWebService.cs
+++ WebClient/Services/IWebService.cs
@@ -4,12 +4,36 @@ namespace WebClient.Services
 {
     public interface IWebService
     {
+        /// <summary>
+        /// Sends a GET request to a specified URL and returns the response as a string asynchronously
+        /// </summary>
+        /// <param name="url">The Url to request to</param>
+        /// <returns>A string containing the response body</returns>
         public Task<string> GetAsync(string url);
 
+        
+        /// <summary>
+        /// Sends a POST request to a specified URL as a string and returns the response body as a string asynchronously
+        /// </summary>
+        /// <param name="url">The Url to request to</param>
+        /// <param name="body">The string data that will be sent as the request body</param>
+        /// <returns>A string containing the response body</returns>
         public Task<string> PostAsync(string url, string body);
 
+        
+        /// <summary>
+        /// Sends a POST request to a specified URL as JSON and returns the response body as a string asynchronously
+        /// </summary>
+        /// <param name="url">The Url to request to</param>
+        /// <param name="body">The string data that will be sent as the request body</param>
+        /// <returns>A string containing the response body</returns>
         public Task<string> PostAsJsonAsync(string url, string body);
 
+        /// <summary>
+        /// Sends a GET request to a specified URL and returns the response as a byte array asynchronously
+        /// </summary>
+        /// <param name="url">The Url to request to</param>
+        /// <returns>A byte array containing the response body</returns>
         public Task<byte[]> GetAsByteArrayAsync(string url);
     }
 }
