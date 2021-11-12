d73e9e8f0aec6fb60bff1b2a4809c6b729c2a68c
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-11 21:10:42 -0600

    Add download command implementation

--- WebClient/Commands/DownloadCommand.cs
+++ WebClient/Commands/DownloadCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using WebClient.Services;
 
@@ -17,6 +19,23 @@ namespace WebClient.Commands
 
         public override async Task<int> ExecuteAsync(CommandContext context, DownloadCommandSettings settings)
         {
+            try
+            {
+                await AnsiConsole.Status()
+                    .StartAsync("[darkorange3_1]Connecting[/]", async ctx =>
+                    {
+                        ctx.Spinner(Spinner.Known.Dots);
+                        var response = await _webService.GetAsByteArrayAsync(settings.Url);
+                        await _fileService.SaveAsync(settings.OutputFile, response);
+                        AnsiConsole.MarkupLine($"[gold3_1]Response saved on {settings.OutputFile}![/]");
+                    });
+            }
+            catch (Exception e)
+            {
+                AnsiConsole.MarkupLine($"[red]{e.Message}[/]");
+                return 1;
+            }
+
             return 0;
         }
     }
